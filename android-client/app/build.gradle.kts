b5a68dcdf5c7b71647d99e85810c5b51e931c16f
Ben Archer <ben.archer2691@gmail.com>
2025-11-14 11:41:53 +0100

    chore: bump Android client to v0.0.45

--- android-client/app/build.gradle.kts
+++ android-client/app/build.gradle.kts
@@ -65,8 +65,8 @@ android {
         applicationId = "com.bbtec.mdm.client"
         minSdk = 29
         targetSdk = 34
-        versionCode = 44
-        versionName = "0.0.44"
+        versionCode = 45
+        versionName = "0.0.45"
 
         // BUILD PROVENANCE - Inject git metadata into APK
         // Access these in your app via BuildConfig.GIT_COMMIT_SHA, etc.
